749b5933e4915b0379a2136bc48b0d69cb9eb88b
felipe.dev <083felype@gmail.com>
2025-04-16 21:01:24 -0300

    feat(migrations): update id column type to INTEGER in state, city, and address tables

--- src/migration/1744308425383-create_table_state.ts
+++ src/migration/1744308425383-create_table_state.ts
@@ -3,20 +3,19 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateTableState1744308425383 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-          CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-    
-          CREATE TABLE public.state (
-            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-            name VARCHAR NOT NULL,
-            created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
-            updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL
-          );
-        `);
+      CREATE TABLE public.state (
+        id INTEGER PRIMARY KEY,            
+        uf VARCHAR(2) NOT NULL,           
+        name VARCHAR NOT NULL,
+        created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+        updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL
+      );
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-          DROP TABLE IF EXISTS public.state;
-        `);
+      DROP TABLE IF EXISTS public.state;
+    `);
   }
 }
--- src/migration/1744308439329-create_table_city.ts
+++ src/migration/1744308439329-create_table_city.ts
@@ -3,22 +3,20 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateTableCity1744308439329 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-          CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-    
-          CREATE TABLE public.city (
-            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-            state_id UUID NOT NULL,
-            name VARCHAR NOT NULL,
-            created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
-            updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
-            CONSTRAINT fk_state FOREIGN KEY (state_id) REFERENCES public.state(id)
-          );
-        `);
+      CREATE TABLE public.city (
+        id INTEGER PRIMARY KEY,            
+        state_id INTEGER NOT NULL,         
+        name VARCHAR NOT NULL,
+        created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+        updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+        CONSTRAINT fk_state FOREIGN KEY (state_id) REFERENCES public.state(id) 
+      );
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-          DROP TABLE IF EXISTS public.city;
-        `);
+      DROP TABLE IF EXISTS public.city;
+    `);
   }
 }
