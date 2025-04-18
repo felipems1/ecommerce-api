7bfce3742140aefd9d89f7b51ad227bc1f4c2c05
felipe.dev <083felype@gmail.com>
2025-04-17 21:52:19 -0300

    feat(state): add cache to get all states request

--- src/state/state.module.ts
+++ src/state/state.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from 'src/cache/cache.module';
 import { StateEntity } from './entities/state.entity';
 import { StateController } from './state.controller';
 import { StateService } from './state.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StateEntity])],
+  imports: [TypeOrmModule.forFeature([StateEntity]), CacheModule],
   controllers: [StateController],
   providers: [StateService],
 })
--- src/state/state.service.ts
+++ src/state/state.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CacheService } from 'src/cache/cache.service';
 import { Repository } from 'typeorm';
 import { StateEntity } from './entities/state.entity';
 
@@ -8,9 +9,13 @@ export class StateService {
   constructor(
     @InjectRepository(StateEntity)
     private readonly stateRepository: Repository<StateEntity>,
+
+    private readonly cacheService: CacheService,
   ) {}
 
   async getAllState(): Promise<StateEntity[]> {
-    return this.stateRepository.find();
+    return this.cacheService.getCache<StateEntity[]>('states_all', () =>
+      this.stateRepository.find(),
+    );
   }
 }
